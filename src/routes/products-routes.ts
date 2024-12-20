86a155e04e711ad56a10aeb051eb8aec06f34d68
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-12-20 17:34:38 -0300

    chore: confg routes, controllers, AppError

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,8 @@
+import { ProductController } from "@/controllers/products-controller";
+import { Router } from "express";
+
+export const productsRoutes = Router();
+
+const productsController = new ProductController();
+
+productsRoutes.get("/", productsController.index);
